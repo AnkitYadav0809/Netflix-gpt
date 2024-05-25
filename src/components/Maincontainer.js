ad88b7f73173c3753f89533f7162d4fd91f562ad
Ankit Yadav <ankityadav60062@gmail.com>
2024-05-25 10:14:06 +0530

    files added

---
+++ src/components/Maincontainer.js
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import VideoTitle from "./VideoTitle";
+import VideoBackground from "./VideoBackground";
+
+const MainContainer = () => {
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+  //   console.log(movies);
+  if (!movies) return;
+  // const randomIndex = Math.floor(Math.random() * movies.length + 1);
+
+  const mainMovie = movies[0];
+
+  const { original_title, overview, id } = mainMovie;
+
+  return (
+    <div className="pt-[30%] bg-black md:pt-0">
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground movieId={id} />
+    </div>
+  );
+};
+
+export default MainContainer;
