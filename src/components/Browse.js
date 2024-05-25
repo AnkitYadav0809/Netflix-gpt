8a7134c7c4aa96237b6af049980a73b6394d238f
Ankit Yadav <ankityadav60062@gmail.com>
2024-05-25 09:02:57 +0530

    commit

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import Header from './Header'
+import { useSelector } from "react-redux";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import usePopularMovies from "../hooks/usePopularMovies";
+import usePopularTVSeries from "../hooks/usePopularTVSeries";
+import useTopRatedMovies from "../hooks/useTopRatedMovies";
+import useTopRatedTVSeries from "../hooks/useTopRatedTVSeries";
+import useUpComingMovies from "../hooks/useUpComingMovies";
+import GptSearch from "./GptSearch";
+import Header from "./Header";
+import MainContainer from "./Maincontainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+
+  useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatedMovies();
+  useUpComingMovies();
+  usePopularTVSeries();
+  useTopRatedTVSeries();
+
   return (
     <div>
-      <Header/>
+      <Header />
+      {showGptSearch ? (
+        <GptSearch />
+      ) : (
+        <>
+          <MainContainer />
+          <SecondaryContainer />
+        </>
+      )}
     </div>
   );
 };
 
-export default Browse
+export default Browse;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,92 @@
-import React from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../utils/firebase";
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO, SUPPORTED_LANGUAGES, USER_AVATAR } from "../utils/constants";
+import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
-  return (
-    <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
-    
-      <img className='w-44'
-       src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
-   alt='logo'/>
-   <div className='flex p-2'>
-    <img className='w-12 h-12 ' alt='usericon' src="https://occ-0-4875-2164.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229" />
-   <button className='font-bold text-white'>Sign Out</button>
-    </div> </div>
-  )
-}
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-export default Header
+  const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+    // unsubscribe will be called when component unmounts
+    return () => unsubscribe();
+  }, []);
+
+  const handleGPTSearchClick = () => {
+    //Togggle GPT Search
+    dispatch(toggleGptSearchView());
+  };
+
+  const handleLanguageChange = (event) => {
+    dispatch(changeLanguage(event.target.value));
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {})
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
+  return (
+    <div className="absolute w-screen px-8 py-2 pl-12 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
+      <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="Netfix-logo" />
+
+      {user && (
+        <div className="flex p-2 justify-between">
+          {showGptSearch && (
+            <select
+              onChange={handleLanguageChange}
+              className="p-2 m-2 bg-gray-900 text-white"
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="py-2 px-4 mx-4 my-2 bg-green-400 text-white rounded-md hover:bg-green-500"
+            onClick={handleGPTSearchClick}
+          >
+            {showGptSearch ? "Home" : "GPT Search"}
+          </button>
+          {/* <img
+            className="hidden md:block w-12 h-12"
+            src={USER_AVATAR}
+            alt="user icon"
+          /> */}
+          <button
+            onClick={handleSignOut}
+            className="px-4 py-2 mx-4 my-2 bg-red-600 text-white rounded sm:font-bold "
+          >
+            Sign out
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
