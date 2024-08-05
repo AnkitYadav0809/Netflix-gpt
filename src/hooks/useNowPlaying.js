617268a6fcdf6ade06791d551a53fc2163316ec7
Ankit Yadav <ankityadav60062@gmail.com>
2024-08-05 19:07:00 +0530

    comt

--- src/hooks/useNowPlaying.js
+++ src/hooks/useNowPlaying.js
@@ -12,7 +12,7 @@ const useNowPlaying = () => {
       api_options
     );
     const json = await data.json();
-    // console.log(json.results);
+   
     dispatch(addNowPlayingmovies(json.results));
   };
 
